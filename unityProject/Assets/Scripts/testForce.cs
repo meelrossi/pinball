b388691e7c5a0d76d6d309d69ea136feb223c792
meelrossi <merossi@itba.edu.ar>
2017-09-24 16:00:48 -0300

    working simple pinball

---
+++ unityProject/Assets/Scripts/testForce.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testForce : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.UpArrow)) {
+			Impluse ();
+		}
+	}
+
+	void Impluse()
+	{
+		GetComponent<Rigidbody>().AddForce(-transform.up * 30, ForceMode.Impulse);
+		GetComponent<Rigidbody>().useGravity = true;
+	}
+}
